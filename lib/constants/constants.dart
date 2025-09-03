686423ffdda8712cbfb00893e9ad109d6ec6bde1
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-03 06:22:43 +0300

    Add color constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+abstract class ColorsStyles {
+  static const int primaryColor = 0xFFFF9500;
+  static const int secondaryColor = 0xFF505050;
+  static const int greyColor = 0xFFD4D4D2;
+  static const int scaffoldDarkBackgroundColor = 0xFF17181A;
+  static const int scaffoldLightBackgroundColor = 0xFFFBFBFD;
+}
