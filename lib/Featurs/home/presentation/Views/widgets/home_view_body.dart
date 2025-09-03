19ea36c3ea925aa2b3ca526a014f11e7cad8bbdf
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-03 09:14:26 +0300

    Add SVG icon to home view body

--- lib/Featurs/home/presentation/Views/widgets/home_view_body.dart
+++ lib/Featurs/home/presentation/Views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:calculator/Featurs/home/presentation/Views/widgets/button_section.dart';
 import 'package:calculator/Featurs/home/presentation/Views/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -12,7 +13,11 @@ class HomeViewBody extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.end,
-        children: [CustomTextField(), ButtonsSection()],
+        children: [
+          SvgPicture.asset('assets/icons/Vector.svg', width: 30),
+          CustomTextField(),
+          ButtonsSection(),
+        ],
       ),
     );
   }
