c3e835f4c7650d851ef139ebed6274cf8a0f4b86
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-03 06:13:40 +0300

    Add text styles for Poppins font

---
+++ lib/Views/styles.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const TextStyle textStyle48 = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 48,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyle32 = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 32,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyle28 = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyle24 = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyle15 = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 15,
+    fontWeight: FontWeight.w500,
+  );
+}
