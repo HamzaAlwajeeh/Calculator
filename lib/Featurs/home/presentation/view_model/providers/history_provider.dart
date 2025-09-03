d05b8405ea142d66972a4547880a27f2b64514ee
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-03 12:37:48 +0300

    Refactor history management to utilize Hive for persistent storage

--- lib/Featurs/home/presentation/view_model/providers/history_provider.dart
+++ lib/Featurs/home/presentation/view_model/providers/history_provider.dart
@@ -1,20 +1,27 @@
 import 'package:calculator/Featurs/home/data/models/history_model.dart';
+import 'package:calculator/constants/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HistoryProvider extends ChangeNotifier {
   List<HistoryModel> histories = [];
   bool isShowingHistory = false;
 
-  void addHistory(HistoryModel history) {
-    histories.add(history);
+  void addHistory(HistoryModel history) async {
+    var historyBox = Hive.box<HistoryModel>(kHistoryBox);
+    await historyBox.add(history);
+  }
+
+  void fechHistories() {
+    var historyBox = Hive.box<HistoryModel>(kHistoryBox);
+    histories = historyBox.values.toList();
     notifyListeners();
   }
 
   void showingHistory() {
     isShowingHistory = !isShowingHistory;
     if (isShowingHistory) {
-      //TODO => fetch
-      notifyListeners();
+      fechHistories();
     } else {
       histories.clear();
       notifyListeners();
